629ca10c3bb4310812ba6d3c51ac256db05f4a26
Yashika <139475047+Yashika-Developer@users.noreply.github.com>
2023-12-13 15:28:14 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArxV3wYNCr5yosyCMTDf9A5-r9HU6DO1w",
+  authDomain: "mind-mingle-90a55.firebaseapp.com",
+  projectId: "mind-mingle-90a55",
+  storageBucket: "mind-mingle-90a55.appspot.com",
+  messagingSenderId: "720877486605",
+  appId: "1:720877486605:web:05e63b1747f30ca91a8b44"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
