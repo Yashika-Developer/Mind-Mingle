cc97d62be7f5075cbad114970fa518fae2cd9b86
Yashika <139475047+Yashika-Developer@users.noreply.github.com>
2023-12-13 15:30:14 +0530

    Delete Backend directory

--- Backend/router/mail.js
+++
@@ -1,8 +0,0 @@
-exports.generateOTP = ()=>{
-          let OTP = '';
-          for (let i = 0; i <= 3; i++) {
-                    let ranVal = Math.round(Math.random()*9);
-                    OTP = OTP+ranVal;
-          }
-          return OTP;
-};
